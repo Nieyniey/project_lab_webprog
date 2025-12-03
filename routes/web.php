577c7ce7837258e167e50248c06cbedee62e8595
hyyga <aureliacalistta537@gmail.com>
2025-12-03 09:48:36 +0700

    bener ya

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AirinsControllerAuth;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\PropertyController;
-use App\Http\Controllers\ReviewController;
-
-// ======================
-// Guest Routes
-// ======================
+use App\Http\Controllers\ReviewController; 
 
 // Login & Register
 Route::get('/login', [AirinsControllerAuth::class, 'ShowLogin'])->name('login');
@@ -19,32 +15,21 @@ Route::post('/register', [AirinsControllerAuth::class, 'register'])->name('regis
 
 Route::post('/logout', [AirinsControllerAuth::class, 'Logout'])->name('logout');
 
-// Home
-Route::get('/home', [PropertyController::class, 'home'])->name('home');
-
-// Search
+// Home and Search Page
+Route::get('/', [PropertyController::class, 'home'])->name('home');
 Route::get('/search', [PropertyController::class, 'search'])->name('search');
+Route::get('/property/{id}', [PropertyController::class, 'show'])->name('property.detail');
 
-// ======================
-// Authenticated Routes
-// ======================
-Route::middleware(['auth'])->group(function () {
+// Protected Routes (harus login)
+Route::middleware('auth')->group(function () {
+    // Property Detail Book
+    Route::post('/property/{id}/book', [BookingController::class, 'book'])->name('property.book');
 
-    // Bookings
+    // My Bookings
     Route::get('/mybookings', [BookingController::class, 'index'])->name('mybookings');
     Route::post('/cancel-booking/{id}', [BookingController::class, 'cancel'])->name('cancel.booking');
 
-    // Property Detail
-    Route::get('/property/{id}', [PropertyController::class, 'show'])->name('property.detail');
-
-    // Add Property
-    Route::get('/add-property', [PropertyController::class, 'showAdd'])->name('addProperty');
-    Route::post('/add-property', [PropertyController::class, 'store'])->name('addProperty.post');
-
-    // My Properties
-    Route::get('/my-properties', [PropertyController::class, 'myProperties'])->name('myProperties');
-
-    // Reviews
+    // Review Page 
     Route::get('/review/{id}', [ReviewController::class, 'show'])->name('review.page');
     Route::post('/review/{id}', [ReviewController::class, 'store'])->name('review.submit');
 });
